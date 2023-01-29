7aaab821fd6d50540e77af6c171fea13bd764171
afreenahmad <afreenahmad01@gmail.com>
2023-01-29 18:34:24 -0500

    2

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
-
+import {createUserWithEmailAndPassword } from "firebase/auth"
+import { auth} from "../firebase";
 const Register = () => {
+    const [err,setErr] = useState(false)
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+        const displayName=(e.target[0].value)
+        const  email=(e.target[1].value)
+        const  password=(e.target[2].value)
+        const  file=(e.target[3].files[0])
+
+        //const auth = getAuth();
+        try{
+            const res=  await createUserWithEmailAndPassword(auth, email, password)
+
+        }catch(err){
+            setErr(true)
+
+        }
+        
+        
+
+    }
     return (
         <div className="formContainer">
             <div className="formWrapper">
                 <span className="logo">AfreenChat</span>
                 <span className="title">Register</span>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type = "text" placeholder="display"/>
                     <input type="email" placeholder="email"/>
                     <input type ="password" placeholder="password"/>
@@ -16,6 +38,7 @@ const Register = () => {
 
                     </label>
                     <button>Sign Up</button>
+                    {err && <span>Something went wrong</span>}
                 </form>
                 <p>Have an account? Login</p>
             </div>
